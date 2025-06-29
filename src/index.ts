d08f7a9974712f28f40f97e9a5ec71bf2dda6c20
trnq-eu <48558947+trnq-eu@users.noreply.github.com>
2025-06-29 07:58:34 +0200

    refactored json parsing

--- src/index.ts
+++ src/index.ts
@@ -7,21 +7,21 @@ import {
   middlewareLogResponse,
   middlewareMetricsInc,
 } from "./api/middleware.js";
-import { handlerValidateChirp } from "./api/validate_chirp.js"; // Import the new handler
+import { handlerChirpsValidate } from "./api/chirps.js";
 
 const app = express();
 const PORT = 8080;
 
-app.use(express.json()); // Add middleware to parse JSON request bodies - ADDED BACK
 app.use(middlewareLogResponse);
-app.use("/app", middlewareMetricsInc, express.static("./src/app"));
+app.use(express.json());
 
-// TO-DO: create api routes
+app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 
 app.get("/api/healthz", handlerReadiness);
 app.get("/admin/metrics", handlerMetrics);
 app.post("/admin/reset", handlerReset);
-app.post("/api/validate_chirp", handlerValidateChirp); // Add the new route BACK to original
+
+app.post("/api/validate_chirp", handlerChirpsValidate);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
